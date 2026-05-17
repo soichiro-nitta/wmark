59727d180615646f1ce85ab3d94ecd482ca241b1
Soichiro Nitta <soic1226@gmail.com>
2026-05-17 16:57:40 +0900

    ウィンドウの角丸を強める

--- Sources/wmark-app/main.swift
+++ Sources/wmark-app/main.swift
@@ -421,7 +421,7 @@ struct ContentView: View {
                     .padding(.trailing, 16)
             }
         }
-        .clipShape(RoundedRectangle(cornerRadius: 18, style: .continuous))
+        .clipShape(RoundedRectangle(cornerRadius: 28, style: .continuous))
     }
 }
 
